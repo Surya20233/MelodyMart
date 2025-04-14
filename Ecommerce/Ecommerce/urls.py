7116d7d837855c08707e777d8c081506b22a8dba
Surya Prakash Geddam <suryaprakashgeddam170@gmail.com>
2025-04-14 13:26:03 +0530

    updated static

--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -63,7 +63,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Melody Mart"
 admin.site.site_title = "Melody Mart"
